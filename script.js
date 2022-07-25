594c70e4a7c25ad35477aa8db1d126611958bb2c
Gonzalo Juanes <rage.against.themachine.740@gmail.com>
2022-07-25 20:42:34 -0300

    Desafio entregable

---
+++ script.js
@@ -0,0 +1,17 @@
+function calcularDolar(valor, solidario){
+    valor = prompt("Ingresar valor actual")
+    console.log(valor)
+    solidario = 1.65
+    console.log(valor * solidario)
+    turista = 1.70
+    console.log(valor * turista)
+    netflix = 1.55
+    console.log(valor * netflix)
+    alert (`Valor dolar oficial ${valor}, Dolar solidario${valor * solidario}, Dolar turista ${valor * turista}, Dolar netflix ${valor * netflix}`)
+
+    
+} 
+
+
+
+

6e51e6a5015baa261e5ae5193fa3021a6ad76e56
Gonzalo Juanes <rage.against.themachine.740@gmail.com>
2022-07-25 21:04:15 -0300

    Primer modificación

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-function calcularDolar(valor, solidario){
-    valor = prompt("Ingresar valor actual")
-    console.log(valor)
-    solidario = 1.65
-    console.log(valor * solidario)
-    turista = 1.70
-    console.log(valor * turista)
-    netflix = 1.55
-    console.log(valor * netflix)
-    alert (`Valor dolar oficial ${valor}, Dolar solidario${valor * solidario}, Dolar turista ${valor * turista}, Dolar netflix ${valor * netflix}`)
+let turista = 1.7,
+solidario = 1.65,
+netflix = 1.55;
 
-    
-} 
+function calcularDolar(valor) {
+valor = prompt("Ingresar valor actual");
 
+let valorResultado = alert(
+  `Valor dolar oficial ${valor}, Dolar solidario${
+    valor * solidario
+  }, Dolar turista ${valor * turista}, Dolar netflix ${valor * netflix}`
+);
+return valorResultado;
+}
 
-
+calcularDolar();
 
